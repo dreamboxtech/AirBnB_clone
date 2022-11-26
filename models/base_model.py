4e38be58bbe7c4a4f6972fbfac0e953b76f9f396
dreamboxtech <bbbolaleye@gmail.com>
2022-11-26 19:12:20 +0100

    sf

--- models/base_model.py
+++ models/base_model.py
@@ -20,20 +20,17 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initializes the base model"""
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(
-                            value,
-                            '%Y-%m-%dT%H:%M:%S.%f')
-                elif key == "__class__":
-                    continue
-
-                setattr(self, key, value)
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
@@ -42,8 +39,8 @@ class BaseModel:
             readable format of
             an object
         """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        class_res = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_res, self.id, self.__dict__)
 
     def save(self):
         """Update updated_at with the current datetime."""
